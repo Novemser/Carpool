25b639e4a412373f294ea4beee425ca6f51aed39
Novemser <2072806652@qq.com>
2016-11-28 20:21:49 +0800

    修复了Spring配置文件的问题，修改了文件夹组织结构

--- src/main/Java/com/carpool/website/dao/RoomEntityRepository.java
+++ src/main/Java/com/carpool/website/dao/RoomEntityRepository.java
@@ -2,9 +2,13 @@ package com.carpool.website.dao;
 
 import com.carpool.domain.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by qi on 2016/11/27.
  */
+@Component
+@Repository
 public interface RoomEntityRepository extends JpaRepository<RoomEntity, Integer> {
 }
