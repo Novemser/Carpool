3e576fa211e0b6792d85ead4cb926a5b69dd4e8c
Novemser <2072806652@qq.com>
2016-11-28 18:38:30 +0800

    WTF

--- src/main/Java/com/carpool/website/dao/JourneyEntityRepository.java
+++ src/main/Java/com/carpool/website/dao/JourneyEntityRepository.java
@@ -1,10 +1,12 @@
 package com.carpool.website.dao;
 
+import com.carpool.domain.JourneyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by qi on 2016/11/26.
  */
 @Repository
-public interface JourneyEntityRepository extends Repository {
+public interface JourneyEntityRepository extends JpaRepository<JourneyEntity, Integer> {
 }
--- src/main/Java/com/carpool/website/dao/RoomEntityRepository.java
+++ src/main/Java/com/carpool/website/dao/RoomEntityRepository.java
@@ -1,7 +1,10 @@
 package com.carpool.website.dao;
 
+import com.carpool.domain.RoomEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by qi on 2016/11/27.
  */
-public interface RoomEntityRepository {
+public interface RoomEntityRepository extends JpaRepository<RoomEntity, Integer> {
 }
