b8e43b2249135e6ffb330241dd140b1fdc000360
Novemser <2072806652@qq.com>
2016-11-29 17:27:09 +0800

    Modify repos

--- src/main/java/com/carpool/website/dao/UserEntityRepository.java
+++ src/main/java/com/carpool/website/dao/UserEntityRepository.java
@@ -1,16 +1,13 @@
 package com.carpool.website.dao;
 
 import com.carpool.domain.UserEntity;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by qi on 2016/11/26.
  */
-@ComponentScan
-@Component
+
 @Repository
 public interface UserEntityRepository extends JpaRepository<UserEntity,String>{
 
