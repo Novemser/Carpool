be66ca3627d0e298a31a3b7df7af89bcf2ee5673
Novemser <2072806652@qq.com>
2016-11-29 17:18:59 +0800

    Modify

--- src/main/java/com/carpool/website/dao/RoomEntityRepository.java
+++ src/main/java/com/carpool/website/dao/RoomEntityRepository.java
@@ -2,10 +2,14 @@ package com.carpool.website.dao;
 
 import com.carpool.domain.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by qi on 2016/11/27.
  */
-public interface RoomEntityRepository extends JpaRepository<RoomEntity,Integer> {
+@Component
+@Repository
+public interface RoomEntityRepository extends JpaRepository<RoomEntity, Integer> {
 
 }
