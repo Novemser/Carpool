76c61fe9b3ffd6138aa24b4f000bc9151fd8412b
DESKTOP-4PLSSQM\deado <deadoggy@aliyun.com>
2016-11-30 16:26:37 +0800

    UserRepo类和ChatRecordRepo类写完了，引入了加密Service, UserEntity添加了构造函数。 数据库中只存摘要，今后对比密码需要调用加密Service的方法

---
+++ src/main/java/com/carpool/domain/ChatRecordEntity.java
@@ -0,0 +1,92 @@
+package com.carpool.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by qi on 2016/11/26.
+ */
+@Entity
+@Table
+public class ChatRecordEntity {
+    private int id;
+    private Date commentime;
+    private String commenttext;
+    private UserEntity sender;
+    private RoomEntity room;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "commenttime", nullable = false)
+    public Date getCommentime() {
+        return commentime;
+    }
+
+    public void setCommentime(Date commentTime) {
+        this.commentime = commentTime;
+    }
+
+    @Basic
+    @Column(name = "commenttext", nullable = false)
+    public String getCommenttext() {
+        return commenttext;
+    }
+
+    public void setCommenttext(String commentText) {
+        this.commenttext = commentText;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChatRecordEntity that = (ChatRecordEntity) o;
+
+        if (id != that.id) return false;
+        if (commentime != null ? !commentime.equals(that.commentime) : that.commentime != null) return false;
+        if (commenttext != null ? !commenttext.equals(that.commenttext) : that.commenttext != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (commentime != null ? commentime.hashCode() : 0);
+        result = 31 * result + (commenttext != null ? commenttext.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "senderid", referencedColumnName = "id")
+    public UserEntity getSender() {
+        return sender;
+    }
+
+    public void setSender(UserEntity sourceUser) {
+        this.sender = sourceUser;
+    }
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "roomid", referencedColumnName = "id")
+    public RoomEntity getRoom() {
+        return room;
+    }
+
+    public void setRoom(RoomEntity room) {
+        this.room = room;
+    }
+
+}
---
+++ src/main/java/com/carpool/website/dao/UserEntityRepository.java
@@ -0,0 +1,54 @@
+package com.carpool.website.dao;
+
+import com.carpool.domain.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by qi on 2016/11/26.
+ */
+
+@Repository
+public interface UserEntityRepository extends JpaRepository<UserEntity, String> {
+    public UserEntity findById(String id);
+    public Page<UserEntity> findByUsername(String username);
+    public Page<UserEntity> findByQqAcount(String acount);
+    public Page<UserEntity> findByWechatAccount(String acount);
+    public Integer          countIdByGender(byte gender);
+
+    @Query("select user.coins from UserEntity user where user.id=?1")
+    public Integer getCoinsById(String id);
+
+    @Query("select user.alipay from UserEntity user where user.id=?1")
+    public String getAlipayById(String id);
+
+    @Query("select user.qqAccount from UserEntity user where user.id=?1")
+    public String getQQAccountById(String id);
+
+    @Query("select user.wechatAccount from UserEntity user where user.id=?1")
+    public String getWechatByid(String id);
+
+    @Query("select user.password from UserEntity user where user.id=?1")
+    public String getPwById(String id);
+
+    @Query("select user.gender from UserEntity user where user.id=?1")
+    public Byte getGenderBuId(String id);
+
+    @Query("select user.credit from UserEntity user where user.id=?1")
+    public Double getCreditById(String id);
+
+    @Query("delete from UserEntity user where user.id=?1")
+    public Integer deleteUserById(String id);
+
+    @Query("update UserEntity user set user.username=?1 where user.id=?2")
+    public Integer setUsername(String newName, String id);
+
+    @Query("update UserEntity user set user.alipay =?1 where user.id=?2")
+    public Integer setUserAlipay(String newAlipay, String id);
+
+    @Query("update UserEntity user set user.password=?1 where user.id=?2")
+    public Integer setUserPw(String newPw, String id);
+
+}
